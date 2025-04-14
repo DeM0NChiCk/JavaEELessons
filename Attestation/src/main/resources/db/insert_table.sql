e7e3545a2bb8a6ac0bf1a03a34977ac4f4978ff8
ANewcomertoJava <iit8610@gmail.com>
2025-04-14 21:41:31 +0300

    ### Домашнее задание 18.03.2025 - Книжный магазин - Требования: Поднять базу данных, создать таблицу с сущностями, заполнить данными.

---
+++ Attestation/src/main/resources/db/insert_table.sql
@@ -0,0 +1,13 @@
+INSERT INTO authors (name, bio) VALUES
+        ('Джоан Роулинг', 'Автор Гарри Поттера'),
+        ('Джордж Оруэлл', 'Автор "1984"');
+
+INSERT INTO genres (name) VALUES
+        ('Фэнтези'), ('Антиутопия'), ('Роман');
+
+INSERT INTO books (title, author_id, price) VALUES
+        ('Гарри Поттер и философский камень', 1, 500),
+        ('1984', 2, 450);
+
+INSERT INTO book_genres (book_id, genre_id) VALUES
+        (1, 1), (1, 3), (2, 2);
